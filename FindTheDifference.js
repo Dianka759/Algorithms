ef2fdf84f9c04f34fb59322203eee233730dc94d
dianka759 <dianka759@gmail.com>
2022-02-07 15:19:13 -0500

    Morning Algorithms

---
+++ FindTheDifference.js
@@ -0,0 +1,27 @@
+//  Find the difference 
+
+var findTheDifference = function (s, t) {
+    sortedS = s.split('').sort();
+    sortedT = t.split('').sort();
+
+    for (let i = 0; i < sortedT.length; i++) {
+        if (sortedS[i] !== sortedT[i]) {
+            return sortedT[i];
+        }
+    }
+};
+
+console.log(findTheDifference("abcd", "abcde"))
+
+
+// const stringDiff = (str1, str2) => {
+//     for (var i = 0; i < str2.length; i++) {
+//         var placer = str1.indexOf(str2[i])
+//         if (placer >= 0) {
+//             str2 = str2.slice(0, i) + str2.slice(i + 1)
+//             str1 = str1.slice(0, placer) + str1.slice(placer + 1)
+//             i--
+//         }
+//     } return str2
+// }
+// console.log(stringDiff("abcd", "abcde"));
---
+++ LengthOfLastWord.js
@@ -0,0 +1,21 @@
+// Find the length of the last word. 
+
+var lengthOfLastWord = function (s) {
+    let startWord = false;
+    let count = 0;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] !== " ") {
+            startWord = true;
+        }
+        if (startWord) {
+            if (s[i] == " ") {
+                return count;
+            } else {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+console.log(lengthOfLastWord("   fly me   to   the moon  "))
